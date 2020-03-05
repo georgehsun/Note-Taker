a95cfedfad4cde17e9417d010beaed1d115b2281
George Sun <Gsunems@gmail.com>
2020-03-05 15:59:55 -0600

    add video

--- develop/server.js
+++ develop/server.js
@@ -1,41 +1,32 @@
-//the path module is a node js module to deal with file paths
-
-
 const express = require("express");
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
-// Initialize the app and create a port
 const app = express();
-const port = 3007;
+const port = 3014;
 const mainDir = path.join(__dirname, "/public");
 
-// Set up body parsing, static, and route middleware
-
-
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//*here im setting up my index.html
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(mainDir, "notes.html"));
 });
 
 app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/db/db.json"));
-})
+});
 
-//*
 app.get("/api/notes/:id", function(req, res) {
     let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     res.json(savedNotes[Number(req.params.id)]);
 });
-//*
+
 app.get("*", function(req, res) {
     res.sendFile(path.join(mainDir, "index.html"));
 });
-//*
+
 app.post("/api/notes", function(req, res) {
     let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     let newNote = req.body;
@@ -48,23 +39,24 @@ app.post("/api/notes", function(req, res) {
     res.json(savedNotes);
 })
 
-//*
 app.delete("/api/notes/:id", function(req, res) {
-        let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-        let noteID = req.params.id;
-        let newID = 0;
-        console.log(`Deleting note with ID ${noteID}`);
-        savedNotes = saveNotes.filter(currNote => {
-            return currNote.id != noteID;
-        })
-        for (currNote of savedNotes) {
-            currNote.id = newID.toString();
-            newID++;
-        }
-        fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
-        res.json(savedNotes);
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteID = req.params.id;
+    let newID = 0;
+    console.log(`Deleting note with ID ${noteID}`);
+    savedNotes = savedNotes.filter(currNote => {
+        return currNote.id != noteID;
     })
-    // Start the server on the port
+
+    for (currNote of savedNotes) {
+        currNote.id = newID.toString();
+        newID++;
+    }
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
+})
+
 app.listen(port, function() {
-    console.log(`Listening on port: ${port}. Enjoy!`);
+    console.log(`Now listening to port ${port}. Enjoy your stay!`);
 })
